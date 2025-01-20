988b33567ea76707a57ed24e4b0b046bab2c4937
Hijaker <30287680+Hijaker@users.noreply.github.com>
2025-01-20 11:46:06 +0100

    Updated connection string

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const mongoose = require("mongoose");
+const { mongoDBPassword } = require("./password.js");
 
-app.listen(PORT, () => {
-    console.log("Listening on 3000");
-});
+
+
+app.get("/", (req, res) => {
+    res.send("Welcome to my MongoAPI")
+})
+
+app.post("/blogpost", (req, res) => {
+    res.send("This is a postrequest")
+})
+
+
+
+
+
+mongoose.
+connect("mongodb+srv://hijaker:${mongoDBPassword}@cluster0.phpb6.mongodb.net/Products-API?retryWrites=true&w=majority&appName=Cluster0")
+.then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+        console.log("Listening on 3000");
+    });
+})
