0bcbd3f1ceafd8f122da3faeba5e952e77fffd79
Hijaker <30287680+Hijaker@users.noreply.github.com>
2025-01-20 15:05:55 +0100

    Final commit of the day

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const PORT = 3000;
 const mongoose = require("mongoose");
-const { mongoDBPassword } = require("./password.js");
-
-
+const Product = require("./Models/productModel.js")
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Welcome to my MongoAPI")
@@ -14,12 +14,52 @@ app.post("/blogpost", (req, res) => {
     res.send("This is a postrequest")
 })
 
+app.post("/api/product", async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        res.status(404);
+    }
+});
 
+app.get("/api/Products", async(req, res) => {
+try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+} catch (error) {
+    res.status(500).json({message:error.message});
+}
+});
 
+app.get("/api/product/:id", async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+})
+
+app.put("/api/product/:id", async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        if(!product){
+            return res.status(400).json({message: "Products does not exist"});
+        }
+    res.status(200).json(product);
+
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+})
 
 
 mongoose.
-connect("mongodb+srv://hijaker:${mongoDBPassword}@cluster0.phpb6.mongodb.net/Products-API?retryWrites=true&w=majority&appName=Cluster0")
+connect(process.env.MONGO_DB_URI)
 .then(() => {
     console.log("Connected to MongoDB");
     app.listen(PORT, () => {
